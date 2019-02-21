bc2b8615e529a78e99ed8720e6ba018d0255a0ca
elias spanos <spanoselias@outlook.com>
2019-02-21 22:15:21 +0200

    1) Hashcode 2019

---
+++ src/main.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public final class main {
+
+    private static char[][] constructTastyPizza(final List<String> lines, int totalRows, int totalColumns) {
+
+        char[][] tastyPizzaArr = new char[totalRows][totalColumns];
+
+        IntStream.range(0, lines.size())
+                .forEach(rowIdx -> {
+                    final String line = lines.get(rowIdx);
+                    IntStream.range(0, lines.get(rowIdx).length())
+                            .forEach(columnIdx ->
+                                    tastyPizzaArr[rowIdx][columnIdx] = line.charAt(columnIdx));
+                });
+
+        return tastyPizzaArr;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        final List<String> lines =
+                Files.readAllLines(Paths.get("a_example.in"), StandardCharsets.UTF_8);
+
+        final String[] initialValues = lines.get(0).split(" ");
+        final int totalRows = Integer.parseInt(initialValues[0]);
+        final int totalColumns = Integer.parseInt(initialValues[1]);
+        final int minIngridientCellsInSlice = Integer.parseInt(initialValues[2]);
+        final int maxTotalNoOfCellsOfSlice = Integer.parseInt(initialValues[3]);
+
+        lines.remove(0);
+        final char[][] tastyPizza = constructTastyPizza(lines, totalRows, totalColumns);
+
+        System.out.println("Eating pizza!");
+
+    }
+}
